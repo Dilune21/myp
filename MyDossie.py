345cd41656fb12f0063d293eb7349a4e35aa1704
Grazielle Nogueira <112127646+Dilune21@users.noreply.github.com>
2024-07-12 09:29:15 -0300

    Add files via upload

---
+++ MyDossie.py
@@ -0,0 +1,20 @@
+# Iserção de dados...
+print("Digite o seu nome:")
+MyName = str(input())
+print("Digite sua idade:")
+MyAge = int(input())
+print("Digite a sua altura:")
+MyHeigt = float(input())
+
+# Declaração de Variáveis...
+MyWigth = 93
+MyBirthDay = 17
+MyBirthplace = "Rio De Janeiro"
+
+# Exibição do conteúdo das variáveis...
+print(f"Meu nome é {MyName}.")
+print(f"Minha Idade é {MyAge}. ")
+print(f"Minha Altura é de {MyHeigt} metros.")
+print(f"Meu peso é de {MyWigth} kg.") 
+print(f"Minha Dia de Nascimento é {MyBirthDay}.")
+print(f"Meu Lugar de Nascimento é {MyBirthplace}.")

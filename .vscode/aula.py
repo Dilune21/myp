9eff47788a7025f9565c71471d47820063226160
Grazielle Nogueira <112127646+Dilune21@users.noreply.github.com>
2024-07-17 11:11:31 -0300

    Atercacao

--- .vscode/aula.py
+++ .vscode/aula.py
@@ -1,3 +1,11 @@
-print("hoje tem inss")
-input()
+# Correção de exercicios...
+nota1 =int(input("digite a nota1:"))
+nota2 =int(input("digite a nota2:"))
+nota3 =int(input("digite a nota3"))
+nota4 =int(input("digite a nota4"))
 
+
+if Média >=6:
+  print("você esta aprovado!")
+else:
+    print("você está reprovado...")
--- .vscode/mytemp.py
+++ .vscode/mytemp.py
@@ -1,7 +1,7 @@
 # Programa para converter temperaturas...
 print("converter de Celsium para Kelvin e Fahrenheit...")
-Celsium = int(input("Digite a temperatura")
-Kelvin = Celsius + 273
+Celsium = int(input("Digite a temperatura"))
+Kelvin = Celsium + 273
 Fahrenheit =(9 / 5 * Celsium) - 32
-print(f"A temperatura em Kelvim será{Kelvim}.")
+print(f"A temperatura em Kelvim será {Kelvin}.")
 print(f"A temperatura em Fahrenheit será{Fahrenheit}.")

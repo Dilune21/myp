df2531a5a5dddd146d7c6e8ffe4d3edd0c461728
Grazielle Nogueira <112127646+Dilune21@users.noreply.github.com>
2024-07-19 08:39:05 -0300

    new project

--- .vscode/aula.py
+++ .vscode/aula.py
@@ -3,9 +3,14 @@ nota1 =int(input("digite a nota1:"))
 nota2 =int(input("digite a nota2:"))
 nota3 =int(input("digite a nota3"))
 nota4 =int(input("digite a nota4"))
-
+Média = (nota1 + nota2 + nota3 +nota4) / 4
 
 if Média >=6:
   print("você esta aprovado!")
-else:
-    print("você está reprovado...")
+else: Média <4: # type: ignore
+  print("você está reprovado...")
+elif Média 4 and 6:
+  print("Recuperação...")
+
+print(f"Sua média é {Média}.")
+print(type(Média))

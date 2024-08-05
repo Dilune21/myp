d4375772ece3fa0916798bb24c0e914ff0f4296a
Grazielle Nogueira <112127646+Dilune21@users.noreply.github.com>
2024-08-05 08:56:09 -0300

    NEWPAGE

---
+++ .vscode/MyForloop.py
@@ -0,0 +1,8 @@
+# Demostração do uso de for/range...
+print("Digite o valor máximo desejado:")
+Numero = int(input())
+
+print("Segue, os números desejados:")
+for X in range (0, Numero):
+    print(X)
+
